1addce9e79b3b5f1af781511cfd8c4da50450263
oMuraro <pedrooliveiramuraro@gmail.com>
2024-09-12 13:37:47 -0300

    front and ers changes

---
+++ view/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Aeronaves</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="model" placeholder="Modelo">
+        <input type="text" name="status" placeholder="Condição">
+        <input type="text" name="seats" placeholder="Quantidade de Assentos">
+        <input type="text" name="location" placeholder="Localização">
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
