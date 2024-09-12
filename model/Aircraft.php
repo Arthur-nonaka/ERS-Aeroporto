7eca51101072ed0c58284c223bf564353729cb11
Arthur <nonakaodaarthur@gmail.com>
2024-09-12 13:46:06 -0300

    aircraft

--- model/Aircraft.php
+++ model/Aircraft.php
@@ -5,7 +5,43 @@ class Aircraft {
     private int $seats;
     private string $location;
 
-    public function __construct() {
-
+    public function __construct(string $status, string $model, int $seats, string $location) {
+        $this->status = $status;
+        $this->model = $model;
+        $this->seats = $seats;
+        $this->location = $location;
     }
+
+    public function getStatus() {
+        return $this->status;
+    }
+
+    public function setStatus($status) {
+        $this->status = $status;
+    }
+
+    public function getModel() {
+        return $this->model;
+    }
+
+    public function setModel($model) {
+        $this->model = $model;
+    }
+
+    public function getSeats() {
+        return $this->seats;
+    }
+
+    public function setSeats($seats) {
+        $this->seats = $seats;
+    }
+
+    public function getLocation() {
+        return $this->location;
+    }
+
+    public function setLocation($location) {
+        $this->location = $location;
+    }
+
 }       
