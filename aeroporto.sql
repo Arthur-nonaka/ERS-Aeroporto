34ae3a5731b026dafac549938bc9ef73ba5b081e
Arthur <nonakaodaarthur@gmail.com>
2024-09-12 13:40:11 -0300

    databse

---
+++ aeroporto.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12-Set-2024 às 18:34
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `aeroporto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aircraft`
+--
+
+CREATE TABLE `aircraft` (
+  `id` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `seats` int(11) NOT NULL,
+  `location` varchar(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `aircraft`
+--
+ALTER TABLE `aircraft`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `aircraft`
+--
+ALTER TABLE `aircraft`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ model/Aircraft.php
@@ -0,0 +1,11 @@
+<?php
+class Aircraft {
+    private string $status;
+    private string $model;
+    private int $seats;
+    private string $location;
+
+    public function __construct() {
+
+    }
+}       
